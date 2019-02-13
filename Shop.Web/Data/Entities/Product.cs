fe7281d2fe0ee99573ef58c3eba73d9faf903774
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-02-13 10:48:45 -0600

    Modify Database

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -7,7 +7,8 @@ namespace Shop.Web.Data.Entities
     public class Product
     {
         public int Id { get; set; }
-
+        [MaxLength(50, ErrorMessage = "El Campo {0} Solo Acepta {1} Caracteres")]
+        [Required]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -17,10 +18,10 @@ namespace Shop.Web.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Availabe?")]
         public bool IsAvailabe { get; set; }
