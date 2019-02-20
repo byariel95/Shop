342d41f0f48195dacb23d243fce4db5200b535d9
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-02-20 09:18:59 -0600

    add API controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController: Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return this.Ok(this.productRepository.GetAll());
+        }
+
+    }
+}
