34527c2b02c342e9f5e1ca4da42efd1c15ad00ee
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-02-25 16:26:11 -0600

    add login and implement Mvvm

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,8 +1,43 @@
 ﻿
 
+
+
 namespace Shop.UIForms.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Xamarin.Forms;
     public class LoginViewModel
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(Login);
+
+        public LoginViewModel()
+        {
+            Email = "byariel";
+            Password = "123";
+        }
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Alert", "You must enter an Email", "Aceptar");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Alert", "You must enter an Password", "Aceptar");
+                return;
+            }
+            if (!this.Email.Equals("byariel") || !this.Password.Equals("123"))
+            {
+                await Application.Current.MainPage.DisplayAlert("Alert", "Your Password or User are incorrect", "Aceptar");
+                return;
+            }
+            await Application.Current.MainPage.DisplayAlert("Yeah!", "Amazing!!", "ok");
+
+        }
     }
 }
