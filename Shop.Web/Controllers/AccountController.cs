edff6b65476c2b9a02f21c6fe27a5e2479e714ef
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-03-13 10:56:45 -0600

    added generation of tokens in api

--- Shop.Web/Controllers/AccountController.cs
+++ Shop.Web/Controllers/AccountController.cs
@@ -9,14 +9,22 @@ namespace Shop.Web.Controllers
     using System.Linq;
     using Data.Entities;
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
+    using System.Security.Claims;
+    using System.IdentityModel.Tokens.Jwt;
+    using System;
+    using Microsoft.IdentityModel.Tokens;
+    using System.Text;
 
     public class AccountController : Controller
     {
         private readonly IUserHelper userHelper;
+        private readonly IConfiguration configuration;
 
-        public AccountController(IUserHelper userHelper)
+        public AccountController(IUserHelper userHelper, IConfiguration configuration )
         {
             this.userHelper = userHelper;
+            this.configuration = configuration;
         }
 
         public IActionResult Login()
@@ -181,6 +189,47 @@ namespace Shop.Web.Controllers
             return this.View(model);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody] LoginViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = await this.userHelper.GetUserByEmailAsync(model.Username);
+                if (user != null)
+                {
+                    var result = await this.userHelper.ValidatePasswordAsync(
+                        user,
+                        model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
+
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(this.configuration["Tokens:Key"]));
+                        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(
+                            this.configuration["Tokens:Issuer"],
+                            this.configuration["Tokens:Audience"],
+                            claims,
+                            expires: DateTime.UtcNow.AddDays(15),
+                            signingCredentials: credentials);
+                        var results = new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo
+                        };
+
+                        return this.Created(string.Empty, results);
+                    }
+                }
+            }
+
+            return this.BadRequest();
+        }
 
 
 
--- Shop.Web/Helpers/UserHelper.cs
+++ Shop.Web/Helpers/UserHelper.cs
@@ -5,17 +5,19 @@ namespace Shop.Web.Helpers
     using System.Threading.Tasks;
     using Data.Entities;
     using Microsoft.AspNetCore.Identity;
-    using Shop.Web.Models;
+    using Models;
 
     public class UserHelper : IUserHelper
     {
         private readonly UserManager<User> userManager;
         private readonly SignInManager<User> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        public UserHelper(UserManager<User> userManager, SignInManager<User> signInManager)
+        public UserHelper(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager <IdentityRole> roleManager)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+            this.roleManager = roleManager;
         }
 
         public async Task<IdentityResult> AddUserAsync(User user, string password)
