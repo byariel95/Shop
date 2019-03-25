795643a50da901e391cf719d619b98d87fec6a6f
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-03-25 12:42:42 -0600

    added user manager and fixed bugs in views

--- Shop.Web/Data/SeedDB.cs
+++ Shop.Web/Data/SeedDB.cs
@@ -1,21 +1,21 @@
 ﻿
 namespace Shop.Web.Data
 {
-    using Entities;
-    using Microsoft.AspNetCore.Identity;
-    using Helpers;
+
     using System;
     using System.Linq;
     using System.Threading.Tasks;
-    using System.Collections.Generic;
+    using Entities;
+    using Helpers;
+    using Microsoft.AspNetCore.Identity;
 
     public class SeedDb
     {
         private readonly DataContext context;
         private readonly IUserHelper userHelper;
-        private Random random;
+        private readonly Random random;
 
-        public SeedDb(DataContext context,IUserHelper userHelper)
+        public SeedDb(DataContext context, IUserHelper userHelper)
         {
             this.context = context;
             this.userHelper = userHelper;
@@ -26,88 +26,130 @@ namespace Shop.Web.Data
         {
             await this.context.Database.EnsureCreatedAsync();
 
-            await this.userHelper.CheckRoleAsync("Admin");
-            await this.userHelper.CheckRoleAsync("Customer");
+            await this.CheckRoles();
 
             if (!this.context.Countries.Any())
             {
-                var cities = new List<City>();
-                cities.Add(new City { Name = "Quetzaltenango" });
-                cities.Add(new City { Name = "Guatemala" });
-                cities.Add(new City { Name = "Solola" });
-                cities.Add(new City { Name = "Mazatenango" });
-                cities.Add(new City { Name = "Peten" });
-                cities.Add(new City { Name = "Jalapa" });
-
-                this.context.Countries.Add(new Country
-                {
-                    Cities = cities,
-                    Name = "Guatemala"
-                });
-
-                await this.context.SaveChangesAsync();
+                await this.AddCountriesAndCitiesAsync();
             }
 
+            await this.CheckUserAsync("luis@gmail.com", "Luis", "Giron", "Customer");
+            await this.CheckUserAsync("sara@gmail.com", "Sara", "Sum", "Customer");
+            var user = await this.CheckUserAsync("byron_1995_@hotmail.com", "Ariel", "Monterroso", "Admin");
 
-
-            var user = await this.userHelper.GetUserByEmailAsync("byron_1995_@hotmail.com");
-            if (user == null)
-            {
-                user = new User
-                {
-                    FirstName = "Ariel",
-                    LastName = "Monterroso",
-                    Email = "byron_1995_@hotmail.com",
-                    UserName = "byron_1995_@hotmail.com",
-                    PhoneNumber ="54513968",
-                    Address = "4ta Calle 24-56",
-                    CityId = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault().Id,
-                    City = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault()
-
-                };
-
-                var result = await this.userHelper.AddUserAsync(user, "123456");
-                if (result != IdentityResult.Success)
-                {
-                    throw new InvalidOperationException("Could not create the user in seeder");
-                }
-
-                await this.userHelper.AddUserToRoleAsync(user, "Admin");
-                var token = await this.userHelper.GenerateEmailConfirmationTokenAsync(user);
-                await this.userHelper.ConfirmEmailAsync(user, token);
-
-            }
-
-            var isInRole = await this.userHelper.IsUserInRoleAsync(user, "Admin");
-            if (!isInRole)
-            {
-                await this.userHelper.AddUserToRoleAsync(user, "Admin");
-            }
-
-
-
+            // Add products
             if (!this.context.Products.Any())
             {
-                this.AddProduct("Iphone X",user);
-                this.AddProduct("Mouse Magic",user);
-                this.AddProduct("Iwatch Series 4",user);
-                this.AddProduct("Xiaomi Mi 8",user);
+                this.AddProduct("AirPods", 159, user);
+                this.AddProduct("Xiaomi Mi 8", 1199, user);
+                this.AddProduct("Camera Dvr", 799, user);
+                this.AddProduct("HDD 2tb", 1398, user);
+                this.AddProduct("iPhone X", 749, user);
+                this.AddProduct("iWatch Series 4", 399, user);
+                this.AddProduct("Mac Book Air", 789, user);
+                this.AddProduct("Mac Book Pro", 1299, user);
+                this.AddProduct("Mac Mini", 708, user);
+                this.AddProduct("One Plus 6t", 2300, user);
+                this.AddProduct("Magic Mouse", 47, user);
+                this.AddProduct("Asus Strix", 140, user);
+                this.AddProduct("Strix 7.1", 145, user);
+                this.AddProduct("Alienware 17 R5 1060", 67.67M, user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name, User user)
+        private async Task<User> CheckUserAsync(string userName, string firstName, string lastName, string role)
+        {
+            // Add user
+            var user = await this.userHelper.GetUserByEmailAsync(userName);
+            if (user == null)
+            {
+                user = await this.AddUser(userName, firstName, lastName, role);
+            }
+
+            var isInRole = await this.userHelper.IsUserInRoleAsync(user, role);
+            if (!isInRole)
+            {
+                await this.userHelper.AddUserToRoleAsync(user, role);
+            }
+
+            return user;
+        }
+
+        private async Task<User> AddUser(string userName, string firstName, string lastName, string role)
+        {
+            var user = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = userName,
+                UserName = userName,
+                Address = "4ta Calle 24-56",
+                PhoneNumber = "54513968",
+                CityId = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault().Id,
+                City = this.context.Countries.FirstOrDefault().Cities.FirstOrDefault()
+            };
+
+            var result = await this.userHelper.AddUserAsync(user, "123456");
+            if (result != IdentityResult.Success)
+            {
+                throw new InvalidOperationException("Could not create the user in seeder");
+            }
+
+            await this.userHelper.AddUserToRoleAsync(user, role);
+            var token = await this.userHelper.GenerateEmailConfirmationTokenAsync(user);
+            await this.userHelper.ConfirmEmailAsync(user, token);
+            return user;
+        }
+
+        private async Task AddCountriesAndCitiesAsync()
+        {
+            this.AddCountry("Guatemala", new string[] { "Quetzaltenango", "Guatemala", "Sacatepequez", "Peten", "Jalapa", "Mazatenango", "Izabal" });
+            this.AddCountry("Argentina", new string[] { "Córdoba", "Buenos Aires", "Rosario", "Tandil", "Salta", "Mendoza" });
+            this.AddCountry("Estados Unidos", new string[] { "New York", "Los Ángeles", "Chicago", "Washington", "San Francisco", "Miami", "Boston" });
+            this.AddCountry("Ecuador", new string[] { "Quito", "Guayaquil", "Ambato", "Manta", "Loja", "Santo" });
+            this.AddCountry("Peru", new string[] { "Lima", "Arequipa", "Cusco", "Trujillo", "Chiclayo", "Iquitos" });
+            this.AddCountry("Chile", new string[] { "Santiago", "Valdivia", "Concepcion", "Puerto Montt", "Temucos", "La Sirena" });
+            this.AddCountry("Uruguay", new string[] { "Montevideo", "Punta del Este", "Colonia del Sacramento", "Las Piedras" });
+            this.AddCountry("Bolivia", new string[] { "La Paz", "Sucre", "Potosi", "Cochabamba" });
+            this.AddCountry("Venezuela", new string[] { "Caracas", "Valencia", "Maracaibo", "Ciudad Bolivar", "Maracay", "Barquisimeto" });
+            this.AddCountry("Paraguay", new string[] { "Asunción", "Ciudad del Este", "Encarnación", "San  Lorenzo", "Luque", "Areguá" });
+            this.AddCountry("Brasil", new string[] { "Rio de Janeiro", "São Paulo", "Salvador", "Porto Alegre", "Curitiba", "Recife", "Belo Horizonte", "Fortaleza" });
+            this.AddCountry("Panamá", new string[] { "Chitré", "Santiago", "La Arena", "Agua Dulce", "Monagrillo", "Ciudad de Panamá", "Colón", "Los Santos" });
+            this.AddCountry("México", new string[] { "Guadalajara", "Ciudad de México", "Monterrey", "Ciudad Obregón", "Hermosillo", "La Paz", "Culiacán", "Los Mochis" });
+            await this.context.SaveChangesAsync();
+        }
+
+        private void AddCountry(string country, string[] cities)
+        {
+            var theCities = cities.Select(c => new City { Name = c }).ToList();
+            this.context.Countries.Add(new Country
+            {
+                Cities = theCities,
+                Name = country
+            });
+        }
+
+        private async Task CheckRoles()
+        {
+            await this.userHelper.CheckRoleAsync("Admin");
+            await this.userHelper.CheckRoleAsync("Customer");
+        }
+
+        private void AddProduct(string name, decimal price, User user)
         {
             this.context.Products.Add(new Product
             {
                 Name = name,
-                Price = this.random.Next(100),
+                Price = price,
                 IsAvailabe = true,
                 Stock = this.random.Next(100),
-                User = user
+                User = user,
+                ImageUrl = $"~/images/Products/{name}.png"
             });
         }
     }
+
 
 
 }
--- Shop.Web/Helpers/UserHelper.cs
+++ Shop.Web/Helpers/UserHelper.cs
@@ -2,9 +2,12 @@
 
 namespace Shop.Web.Helpers
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Data.Entities;
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
     using Models;
 
     public class UserHelper : IUserHelper
@@ -109,6 +112,25 @@ namespace Shop.Web.Helpers
         public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
         {
             return await this.userManager.ResetPasswordAsync(user, token, password);
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await this.userManager.Users
+                .Include(u => u.City)
+                .OrderBy(u => u.FirstName)
+                .ThenBy(u => u.LastName)
+                .ToListAsync();
+        }
+
+        public async Task RemoveUserFromRoleAsync(User user, string roleName)
+        {
+            await this.userManager.RemoveFromRoleAsync(user, roleName);
+        }
+
+        public async Task DeleteUserAsync(User user)
+        {
+            await this.userManager.DeleteAsync(user);
         }
 
 
