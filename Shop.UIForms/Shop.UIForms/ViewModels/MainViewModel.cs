160edd9940fc814633c77b6747fda4b50aadbd0b
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-03-27 11:36:08 -0600

    modify products page and loginpage

--- Shop.UIForms/Shop.UIForms/ViewModels/MainViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/MainViewModel.cs
@@ -1,11 +1,16 @@
 ﻿
 using Shop.Common.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Shop.UIForms.ViewModels
 {
     public class MainViewModel
     {
         private static MainViewModel instance;
+
+        public ObservableCollection<MenuItemViewModel> Menus { get; set; }
         public LoginViewModel Login { get; set; }
         public ProductsViewModel Products { get; set; }
 
@@ -15,7 +20,43 @@ namespace Shop.UIForms.ViewModels
         public MainViewModel()
         {
             instance = this;
+            this.LoadMenus();
         }
+
+        private void LoadMenus()
+        {
+            var menus = new List<Menu>
+    {
+        new Menu
+        {
+            Icon = "ic_info",
+            PageName = "AboutPage",
+            Title = "About"
+        },
+
+        new Menu
+        {
+            Icon = "ic_phonelink_setup",
+            PageName = "SetupPage",
+            Title = "Setup"
+        },
+
+        new Menu
+        {
+            Icon = "ic_exit_to_app",
+            PageName = "LoginPage",
+            Title = "Close session"
+        }
+    };
+
+            this.Menus = new ObservableCollection<MenuItemViewModel>(menus.Select(m => new MenuItemViewModel
+            {
+                Icon = m.Icon,
+                PageName = m.PageName,
+                Title = m.Title
+            }).ToList());
+        }
+
         public static MainViewModel GetInstance()
         {
             if (instance==null)
---
+++ Shop.UIForms/Shop.UIForms/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Shop.UIForms.ViewModels
+{
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Views;
+    using Xamarin.Forms;
+
+    public class MenuItemViewModel : Common.Models.Menu
+    {
+
+        
+
+        public ICommand SelectMenuCommand => new RelayCommand(this.SelectMenu);
+
+        private async void SelectMenu()
+        {
+            App.Master.IsPresented = false;
+            var mainViewModel = MainViewModel.GetInstance();
+
+            switch (this.PageName)
+            {
+                case "AboutPage":
+                    await App.Navigator.PushAsync(new AboutPage());
+                    break;
+                case "SetupPage":
+                    await App.Navigator.PushAsync(new SetupPage());
+                    break;
+                default:
+                    MainViewModel.GetInstance().Login = new LoginViewModel();
+                    Application.Current.MainPage = new NavigationPage(new LoginPage());
+                    break;
+            }
+        }
+    }
+
+}
