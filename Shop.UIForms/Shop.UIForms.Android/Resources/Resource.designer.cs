f4450d2dab19e437d2db35465fe7ff99592c005a
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-04-12 14:33:56 -0600

    added icons and publish

--- Shop.UIForms/Shop.UIForms.Android/Resources/Resource.designer.cs
+++ Shop.UIForms/Shop.UIForms.Android/Resources/Resource.designer.cs
@@ -6019,10 +6019,13 @@ namespace Shop.UIForms.Droid
 			public const int icon = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int icon_round = 2130903041;
+			public const int icon_launcher = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int launcher_foreground = 2130903042;
+			public const int icon_round = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int launcher_foreground = 2130903043;
 			
 			static Mipmap()
 			{
