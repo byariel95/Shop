51cf82b0205d9e8469f1d495e49b20fd4d1ff7de
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-05-06 11:48:19 -0600

    added mvvm test concept in xamarin forms

---
+++ FourWays.Core/Services/CalculationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FourWays.Core.Services
+{
+    public class CalculationService : ICalculationService
+    {
+        public decimal TipAmount(decimal subTotal, double generosity)
+        {
+            return subTotal * (decimal)(generosity / 100);
+        }
+    }
+
+}
