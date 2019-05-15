4fa285ab0d8d30eb801be3a2414397a0663a4911
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-05-15 12:51:07 -0600

    finish mvvm cross concept

---
+++ FourWays.FormsCross/FourWays.FormsCross/FormsApp.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace FourWays.FormsCross
+{
+    using Xamarin.Forms;
+
+    public partial class FormsApp : Application
+    {
+        public FormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ FourWays.FormsCross/FourWays.FormsCross/Views/TipView.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace FourWays.FormsCross.Views
+{
+    using Core.ViewModels;
+    using MvvmCross.Forms.Views;
+
+    public partial class TipView : MvxContentPage<TipViewModel>
+    {
+        public TipView()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
