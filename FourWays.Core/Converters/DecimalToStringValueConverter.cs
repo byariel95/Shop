91fdb4717701268b709fadae65de9524a8339857
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-05-15 13:21:59 -0600

    added mvvm converter values

---
+++ FourWays.Core/Converters/DecimalToStringValueConverter.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FourWays.Core.Converters
+{
+    using MvvmCross.Converters;
+    using System;
+    using System.Globalization;
+
+    public class DecimalToStringValueConverter : MvxValueConverter<decimal, string>
+    {
+        protected override string Convert(decimal value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{value:C2}";
+        }
+    }
+
+}
